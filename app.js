485daa8ee8669573994735b908c4b6b850bc39e4
vosscg <yot4106@gmail.com>
2024-07-23 11:03:07 +0800

    Feature: check db

--- app.js
+++ app.js
@@ -10,13 +10,20 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
-// 同步資料庫
-sequelize.sync()
+// 驗證資料庫連接
+sequelize.authenticate()
     .then(() => {
-        console.log('Database connected and synchronized');
+        console.log('Connection has been established successfully.');
+        // 同步模型到資料庫
+        return sequelize.sync({ alter: true });
+    })
+    .then(() => {
+        console.log('Database & tables synchronized!');
+        // 啟動應用程式
     })
     .catch(err => {
-        console.error('Unable to connect to the database:', err);
+        console.error('Unable to connect to the database or synchronize tables:', err);
+        process.exit(1); // 確保在連接或同步失敗時終止應用程式
     });
 
 app.use('/user', userRouter)
